412f25ddf58e3fd0ca4ae1c73bfc5477e81e0363
Eldon Williams <eldon.ew.com@gmail.com>
2023-12-05 23:11:24 -0600

    day-6 (hard coded p2, but yk)

---
+++ day-6/problem2.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const inputRaw = fs.readFileSync('input').toString('ascii');
+
+function isNumeric(e) {
+    return e.match(/[0-9]/) != null;
+}
+
+let races = []
+
+let timeLine = inputRaw.split('\n')[0].split(':');
+timeLine.shift();
+timeLine = timeLine[0].split(' ').filter(v => isNumeric(v));
+timeLine = timeLine.map(v => parseInt(v));
+
+let distanceLine = inputRaw.split('\n')[1].split(':');
+distanceLine.shift();
+distanceLine = distanceLine[0].split(' ').filter(v => isNumeric(v));
+distanceLine = distanceLine.map(v => parseInt(v));
+
+for (let i = 0; i < timeLine.length; i++) {
+    races[i] = [];
+    races[i][0] = timeLine[i];
+    races[i][1] = distanceLine[i];
+}
+
+let margin = 0;
+
+races = [[49877895, 356137815021882]];
+
+for (let i = 0; i < races.length; i++) {
+    const [time, dist] = races[i];
+
+    let waysToWin = 0;
+
+    for (let j = 0; j < time; j++) {
+        let newDist = (time - j) * j;
+
+        if (newDist > dist) waysToWin++;
+    }
+
+    if (margin == 0) {
+        margin = waysToWin;
+    } else {
+        margin *= waysToWin;
+    }
+}
+
+console.log(margin)
